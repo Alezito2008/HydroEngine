69ace0abdaac3e016fde4105f976382e8e765f73
GANDELMAN URIEL <96003020@ort>
2025-09-12 09:10:10 -0300

    VertexBuffer + VertexArray + empezado main(para test)

---
+++ engine/Graphics/VertexBuffer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Hydro
+{
+    class VertexBuffer
+    {
+    public:
+        unsigned int m_RendererID;
+    private:
+        VertexBuffer(const void* data, unsigned int size);
+        ~VertexBuffer();
+
+        void Bind() const;
+        void Unbind() const;
+    };
+}
+
