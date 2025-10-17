0cb45f5e1a4ab8abac791e83fa432f8570b79a24
Alezito2008 <>
2025-10-17 10:35:23 -0300

    Cambiado build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,31 @@
 clear
+
+EXECUTABLE_PATH=false;
+
+case $1 in
+    "") ;;
+    Core)
+        EXECUTABLE_PATH=./build/Core/Core
+        ;;
+    *)
+        echo Target no encontrado
+        exit
+        ;;
+esac
+
 mkdir -p build
 cd build
 cmake ..
 cmake --build .
+
+BUILD_STATUS=$?
+
 cd ..
-./build/Core
+
+if [ $BUILD_STATUS != 0 ]; then
+    exit
+fi
+
+if [ $EXECUTABLE_PATH ]; then
+    $EXECUTABLE_PATH
+fi
